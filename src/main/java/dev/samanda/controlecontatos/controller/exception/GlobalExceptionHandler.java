644cdb23d2e97f0092827dc87d3eca1132c9d877
Samanda Silva <samandasilva99@gmail.com>
2023-11-03 23:13:56 -0300

    Implementação do manipulador de exceções

---
+++ src/main/java/dev/samanda/controlecontatos/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package dev.samanda.controlecontatos.controller.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException){
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException){
+        return new ResponseEntity<>("Resource not found", HttpStatus.NOT_FOUND);
+    }
+
+}
