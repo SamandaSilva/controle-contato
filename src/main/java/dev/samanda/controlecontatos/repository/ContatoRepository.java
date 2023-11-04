edd906dca2fa9e0c95a0bb8e5b25803bf3cb8b31
Samanda Silva <samandasilva99@gmail.com>
2023-11-03 23:01:27 -0300

    Implementação da camada de repositório

---
+++ src/main/java/dev/samanda/controlecontatos/repository/ContatoRepository.java
@@ -0,0 +1,9 @@
+package dev.samanda.controlecontatos.repository;
+
+import dev.samanda.controlecontatos.model.Contato;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
+}
