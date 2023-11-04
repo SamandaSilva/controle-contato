82265c11268e4f90ba8ddfff745c8c11ce46a27b
Samanda Silva <samandasilva99@gmail.com>
2023-11-03 22:34:20 -0300

    Initial commit

---
+++ src/main/java/dev/samanda/controlecontatos/ControleContatosApplication.java
@@ -0,0 +1,13 @@
+package dev.samanda.controlecontatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleContatosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/samanda/controlecontatos/ControleContatosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.samanda.controlecontatos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleContatosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
